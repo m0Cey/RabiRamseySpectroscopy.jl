30ee314eb59907fcb21f93edebb30f4d145d5e55
m0Cey <sciencefloppa@gmail.com>
2023-08-21 21:54:28 +0700

    New fix for codecov + new test

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,6 +2,7 @@ using RabiRamseySpectroscopy
 using Test
 
 @testset "RabiRamseySpectroscopy.jl" begin
+    @test FreeEvol(0).operator == [1 0; 0 1]
     @test probe(StateVector(rand(), rand())) ≤ 1
     @test probe(StateVector(rand(Int), rand(Int))) ≤ 1
 end
