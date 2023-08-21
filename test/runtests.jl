464f8fa698b9a8a80c389cff19e8e8d0ebaa7895
m0Cey <sciencefloppa@gmail.com>
2023-08-21 22:14:47 +0700

    Deleted codecov file

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,7 +2,7 @@ using RabiRamseySpectroscopy
 using Test
 
 @testset "RabiRamseySpectroscopy.jl" begin
-    @test FreeEvol(0).operator == [1 0; 0 1]
+    @test wait!(StateVector(0, 1), FreeEvol(0)).state == [0; 1]
     @test probe(StateVector(rand(), rand())) ≤ 1
     @test probe(StateVector(rand(Int), rand(Int))) ≤ 1
 end
